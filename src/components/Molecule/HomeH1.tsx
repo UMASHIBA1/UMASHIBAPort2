e8fd85a5f8821c2b7d59fff29c529423ba673e36
UMASHIBA <umashiba1@gmail.com>
2019-12-26 01:19:27 +0900

    [MOD]HomeH1にonMouseOverイベントを追加

--- src/components/Molecule/HomeH1.tsx
+++ src/components/Molecule/HomeH1.tsx
@@ -4,11 +4,15 @@ import "../../css/Home/HomeH1.scss";
 interface Props {
   children: ReactNode;
   className?: string;
+  onMouseOverFC?: () => void;
 }
 
 const HomeH1: React.FC<Props> = (props: Props) => {
   return (
-    <h1 className={`home-h1${props.className ? " " + props.className : ""}`}>
+    <h1
+      onMouseOver={props.onMouseOverFC}
+      className={`home-h1${props.className ? " " + props.className : ""}`}
+    >
       {props.children}
     </h1>
   );
