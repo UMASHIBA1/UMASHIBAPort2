647f96a871067c99d930d20c6bbdc02996cd2613
UMASHIBA <umashiba1@gmail.com>
2019-12-25 21:37:04 +0900

    [MOD]WorksAreaにタイトル追加

--- src/components/Organisms/Home/WorksArea.tsx
+++ src/components/Organisms/Home/WorksArea.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import "../../../css/Home/WorksArea.scss";
 import blue_design from "../../../statics/blue_design.svg";
+import HomeH1 from "../../Molecule/HomeH1";
 
 const WorksArea: React.FC = () => {
   return (
     <React.Fragment>
       <div id="works-area">
         <div />
+        <HomeH1 className="works-home-h1">Works</HomeH1>
         <object type="image/svg+xml" data={blue_design} />
       </div>
     </React.Fragment>
