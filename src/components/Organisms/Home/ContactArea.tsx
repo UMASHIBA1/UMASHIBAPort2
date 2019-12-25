24e7e069941bb7bb41693b238a0b1fcca1daf35a
UMASHIBA <umashiba1@gmail.com>
2019-12-25 22:07:14 +0900

    [MOD]TechnologyとContactの見出しを追加

--- src/components/Organisms/Home/ContactArea.tsx
+++ src/components/Organisms/Home/ContactArea.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import "../../../css/Home/ContactArea.scss";
 import green_design from "../../../statics/green_design.svg";
+import HomeH1 from "../../Molecule/HomeH1";
 
 const ContactArea: React.FC = () => {
   return (
     <React.Fragment>
       <div id="contact-area">
         <div />
+        <HomeH1 className="contact-home-h1">Contact</HomeH1>
         <object type="image/svg+xml" data={green_design} />
       </div>
     </React.Fragment>
--- src/components/Organisms/Home/TechnologyArea.tsx
+++ src/components/Organisms/Home/TechnologyArea.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import "../../../css/Home/TechnologyArea.scss";
 import orange_design from "../../../statics/orange_design.svg";
+import HomeH1 from "../../Molecule/HomeH1";
 
 const TechnologyArea: React.FC = () => {
   return (
     <React.Fragment>
       <div id="technology-area">
         <div />
+        <HomeH1 className="technology-home-h1">Technology</HomeH1>
         <object type="image/svg+xml" data={orange_design} />
       </div>
     </React.Fragment>
