cc048e264f03347a0e40b26b9d19398cf111a7ea
UMASHIBA <umashiba1@gmail.com>
2019-12-25 21:26:38 +0900

    [MOD] UMASHIBAPortAreaにタイトルを追加

--- src/components/Organisms/Home/UMASHIBAPortArea.tsx
+++ src/components/Organisms/Home/UMASHIBAPortArea.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "../../../css/Home/UMASHIBAPortArea.scss";
 import pink_design from "../../../statics/pink_design.svg";
+import HomeH1 from "../../Molecule/HomeH1";
 
 const UMASHIBAPortArea: React.FC = () => {
   return (
     <div id="umashiba-port-area">
       <div />
+      <HomeH1 className="home-h1-title">UMASHIBA Port</HomeH1>
       <object type="image/svg+xml" data={pink_design} />
     </div>
   );
