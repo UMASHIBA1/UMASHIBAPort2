ff90d0e6e053a7ec4818f03844d9f8e5ce9dc7fb
UMASHIBA <umashiba1@gmail.com>
2019-12-27 03:05:05 +0900

    [MOD] HomeH1のtextのanimationを書くAreaごとに作成

--- src/components/Molecule/HomeH1.tsx
+++ src/components/Molecule/HomeH1.tsx
@@ -2,21 +2,29 @@ import React, { ReactNode } from "react";
 import "../../css/Home/HomeH1.scss";
 
 interface Props {
-  children: ReactNode;
+  children: string;
   className?: string;
   onMouseOverFC?: () => void;
 }
 
 const HomeH1: React.FC<Props> = (props: Props) => {
+  const titleLength = props.children.length;
+
   return (
     <div
       className={`home-h1-container${
         props.className ? " " + props.className : ""
       }`}
     >
-      <h1 onMouseOver={props.onMouseOverFC} className="home-h1">
-        {props.children}
-      </h1>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={`calc(6.5vw * ${titleLength * 0.6})`}
+        height={`calc(6.5vw*1.4)`}
+      >
+        <text x="0" y="calc(6.5vw*1.2)" fill="none" fontSize="6.5vw">
+          {props.children}
+        </text>
+      </svg>
     </div>
   );
 };
