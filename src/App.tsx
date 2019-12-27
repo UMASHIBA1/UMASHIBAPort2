12c9816266a7492b7ec561fe37735e15759b42b2
UMASHIBA <umashiba1@gmail.com>
2019-12-28 05:50:03 +0900

    [ADD]UMASHIBAPageを作成

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,16 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./components/Pages/Home";
 import "./css/base.scss";
 import reduxStore from "./redux/store";
+import UMASHIBAPage from "./components/Pages/UMASHIBAPage";
 
 const App: React.FC = () => {
   return (
     <Provider store={reduxStore}>
       <BrowserRouter>
         <Switch>
+          <Route path="/umashiba">
+            <UMASHIBAPage />
+          </Route>
           <Route path="/">
             <Home />
           </Route>
---
+++ src/components/Pages/UMASHIBAPage.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ColorPageTitle from "../Atomics/ColorPageTitle";
+
+const UMASHIBAPage = () => {
+  return (
+    <div
+      className="pink-background"
+      style={{ width: "100%", height: "100%", padding: "10px" }}
+    >
+      {/* <ColorPageTitle titleColor="pink">UMASHIBA</ColorPageTitle> */}
+    </div>
+  );
+};
+
+export default UMASHIBAPage;
