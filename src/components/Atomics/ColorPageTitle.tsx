9dcf11ce0b3642c22b44b85dcfbbc6ae3373c079
UMASHIBA <umashiba1@gmail.com>
2019-12-28 05:50:26 +0900

    [ADD] ColorPageTitleを作成

---
+++ src/components/Atomics/ColorPageTitle.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "../../css/ColorPage/ColorPageTitle.scss";
+import { ColorNames } from "../../typing/colors";
+
+interface Props {
+  children: string;
+  titleColor: ColorNames;
+}
+
+const ColorPageTitle: React.FC<Props> = (props: Props) => {
+  return (
+    <div className={`color-page-title accent-${props.titleColor}`}>
+      <span className={`${props.titleColor}`}>{props.children}</span>
+    </div>
+  );
+};
+
+export default ColorPageTitle;
