2efccf0233a03b23bfcad7d4bc00211c7011aa52
UMASHIBA <umashiba1@gmail.com>
2019-12-30 04:29:53 +0900

    [ADD] SkillPageCardを作成

---
+++ src/components/Organisms/ColorPage/Skills/SkillsPageCard.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "../../../../css/ColorPage/Skills/SkillsPageCard.scss";
+import ColorPageCard from "../../../Atomics/ColorPage/ColorPageCard";
+import ColorPageCardTitle from "../../../Atomics/ColorPage/ColorPageCardTitle";
+import ColorPageTag from "../../../Atomics/ColorPage/ColorPageTag";
+
+interface Props {
+  title: string;
+  description: string;
+  tags: string[];
+}
+
+const SkillsPageCard: React.FC<Props> = (props: Props) => {
+  return (
+    <ColorPageCard color="orange">
+      <div className="skills-page-card">
+        <ColorPageCardTitle>{props.title}</ColorPageCardTitle>
+        <div className="skills-page-card-description">{props.description}</div>
+        <div className="skills-page-card-tags">
+          {props.tags.map((tagName, index) => (
+            <ColorPageTag key={index} color={"orange"}>
+              {tagName}
+            </ColorPageTag>
+          ))}
+        </div>
+      </div>
+    </ColorPageCard>
+  );
+};
+
+export default SkillsPageCard;
