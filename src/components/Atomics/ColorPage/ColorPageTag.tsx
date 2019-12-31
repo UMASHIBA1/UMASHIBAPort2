68050c0545cf0e7c97b8b8327714ed6e093361ea
UMASHIBA <umashiba1@gmail.com>
2019-12-31 11:33:32 +0900

    [MOD]ColorPageTagにonClickFCを追加

--- src/components/Atomics/ColorPage/ColorPageTag.tsx
+++ src/components/Atomics/ColorPage/ColorPageTag.tsx
@@ -5,11 +5,15 @@ import { ColorNames } from "../../../typing/colors";
 interface Props {
   children: string;
   color: ColorNames;
+  onClickFC?: () => void;
 }
 
 const ColorPageTag: React.FC<Props> = (props: Props) => {
   return (
-    <div className={`color-page-tag ${props.color}-background`}>
+    <div
+      onClick={props.onClickFC}
+      className={`color-page-tag ${props.color}-background`}
+    >
       <span>{props.children}</span>
     </div>
   );
