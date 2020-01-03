2d425dd85d2023340df2c7e4170324ebde1cae03
UMASHIBA <umashiba1@gmail.com>
2020-01-04 07:35:21 +0900

    [MOD]ReduxStateTypeにskillsPageStateを追加

--- src/typing/redux/reduxState.ts
+++ src/typing/redux/reduxState.ts
@@ -1,5 +1,7 @@
 import { HomeState } from "./home/homeState";
+import { SkillsPageState } from "./skillsPage/skillsPageState";
 
 export default interface ReduxStateType {
   homeState: HomeState;
+  skillsPageState: SkillsPageState;
 }
