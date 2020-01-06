f622c030f080194fc4d14fcbad77040ea5f0dd6c
UMASHIBA <umashiba1@gmail.com>
2020-01-06 16:05:58 +0900

    [MOD]SkillsTagPageにanimationを適用

--- src/components/Pages/SkillsTagPage.tsx
+++ src/components/Pages/SkillsTagPage.tsx
@@ -1,10 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { useHistory, useParams } from "react-router";
 import skillsData from "../../datas/skillsData";
 import { CHANGE_CLICKED_TAG } from "../../redux/constants/skillsPageTypes";
 import { DispatchType } from "../../redux/store";
+import { useTypedSelector } from "../../typing/redux/hooks";
+import ColorPageAppearAnimation from "../Atomics/ColorPage/ColorPageAppearAnimation";
 import ColorPageCloseButton from "../Atomics/ColorPage/ColorPageCloseButton";
+import ColorPageDisappearAnimation from "../Atomics/ColorPage/ColorPageDisappearAnimation";
 import TagPageTitle from "../Atomics/ColorPage/TagPageTitle";
 import SkillsPageCard from "../Organisms/ColorPage/Skills/SkillsPageCard";
 import ColorPageContentSpace from "../Template/ColorPage/ColorPageContentSpace";
@@ -16,8 +19,24 @@ const SkillsTagPage: React.FC = () => {
   const { tagName = "" } = useParams();
   const [willCloseContent, changeWillCloseContent] = useState(false);
   const [willClosePage, changeWillClosePage] = useState(false);
+  const [isAppearStartAnimation, changeIsAppearStartAnimation] = useState(true);
   const dispatch: DispatchType = useDispatch();
-  const flagForInitialize = "initialize";
+  const clickedTag = useTypedSelector(
+    state => state.skillsPageState.clickedTag
+  );
+  const [isInitialRender, changeIsInitialRender] = useState(true);
+
+  if (isInitialRender) {
+    dispatch({ type: CHANGE_CLICKED_TAG, payload: "" });
+    changeIsInitialRender(false);
+  }
+
+  const resetAllState = () => {
+    changeWillCloseContent(false);
+    changeWillClosePage(false);
+    changeIsAppearStartAnimation(true);
+    changeIsInitialRender(true);
+  };
 
   const changeWillcloseContentToTrue = () => {
     changeWillCloseContent(true);
@@ -27,39 +46,63 @@ const SkillsTagPage: React.FC = () => {
     changeWillClosePage(true);
   };
 
+  const disappearStartAnimation = () => {
+    changeIsAppearStartAnimation(false);
+  };
+
   const gotoSkillsPage = () => {
     history.push("/skills");
   };
 
-  useEffect(() => {
-    dispatch({ type: CHANGE_CLICKED_TAG, payload: "" });
-  }, [flagForInitialize]);
+  const gotoNewSkillsTagPage = () => {
+    resetAllState();
+    history.push(`/skillstag/${clickedTag}`);
+  };
 
-  return (
-    <ColorPageSpace color="orange">
-      <ColorPageTopSpace>
-        <TagPageTitle color="orange">{tagName}</TagPageTitle>
-        <ColorPageCloseButton
-          willCollapse={willCloseContent}
-          onClickFC={gotoSkillsPage}
-          color="orange"
-        />
-      </ColorPageTopSpace>
-      <ColorPageContentSpace>
-        {skillsData
-          .filter(data => {
-            return data.tags.some(tag => tag === tagName);
-          })
-          .map(data => (
-            <SkillsPageCard
-              willCollapse={willCloseContent}
-              key={data.title}
-              {...data}
-            />
-          ))}
-      </ColorPageContentSpace>
-    </ColorPageSpace>
-  );
+  if (isAppearStartAnimation) {
+    return (
+      <ColorPageAppearAnimation
+        color="orange"
+        onAnimationEndFC={disappearStartAnimation}
+      />
+    );
+  } else if (willClosePage) {
+    return (
+      <ColorPageDisappearAnimation
+        color="orange"
+        onClosePageFC={
+          clickedTag !== "" ? gotoNewSkillsTagPage : gotoSkillsPage
+        }
+      />
+    );
+  } else {
+    return (
+      <ColorPageSpace color="orange">
+        <ColorPageTopSpace>
+          <TagPageTitle color="orange">{tagName}</TagPageTitle>
+          <ColorPageCloseButton
+            willCollapse={willCloseContent}
+            onClickFC={changeWillcloseContentToTrue}
+            color="orange"
+          />
+        </ColorPageTopSpace>
+        <ColorPageContentSpace>
+          {skillsData
+            .filter(data => {
+              return data.tags.some(tag => tag === tagName);
+            })
+            .map(data => (
+              <SkillsPageCard
+                willCollapse={willCloseContent || clickedTag !== ""}
+                onDisappearFC={changeWillClosePageToTrue}
+                key={data.title}
+                {...data}
+              />
+            ))}
+        </ColorPageContentSpace>
+      </ColorPageSpace>
+    );
+  }
 };
 
 export default SkillsTagPage;
